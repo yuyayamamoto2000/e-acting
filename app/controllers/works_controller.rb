c66b0dee149b73486d00e9c5433a2497ea273e3c
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-23 20:14:45 +0900

    フラッシュメッセージ表示

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,5 @@
 class WorksController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_work, only: [:show, :edit, :update, :destroy]
   def index
     @works = Work.all
