48a126d0b846b92d145d0e84a341f56ae5a63193
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-23 22:14:43 +0900

    ImageUploaderをuserモデルに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  mount_uploader :image, ImageUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
