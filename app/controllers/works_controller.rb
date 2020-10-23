1687d63783fca9006ac4c2b651cb34ac93ebdea6
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-23 18:12:45 +0900

    before_action :authenticate_user!ユーザーがログインしている場合にのみアクセスを許可する

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,5 @@
 class WorksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_work, only: [:show, :edit, :update, :destroy]
   def index
     @works = Work.all
