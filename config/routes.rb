dc08fc81b76a42ad203f357665d5923df5a5f891
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-23 21:40:56 +0900

    routes.rbにget、何か記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'users/show'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'works#index'
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+
+  get "users/show"
 
   resources :works do
     collection do
