885b4ab0dcc0b8715e80670913bcd92f57ffa963
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-24 17:15:16 +0900

    Conversations.controllerにメモ追記

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,11 +3,11 @@ class ConversationsController < ApplicationController
     @conversations = Conversation.all
   end
 
-  def create
-    if Conversation.between(params[:sender_id], params[:recipient_id]).present?#senderが送り主で、recipientが受取人。
-      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+  def create#senderが送り主で、recipientが受取人。
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?#該当のユーザ間での会話が過去に存在しているか（該当ユーザー間で会話をするためのチャットルームがすでに存在しているか）を確認する式
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first#存在した場合、その会話（チャットルーム）情報を取得
     else
-      @conversation = Conversation.create!(conversation_params)
+      @conversation = Conversation.create!(conversation_params)#もし、過去に一件も存在しなかった場合、強制的に会話（チャットルーム）情報を生成
     end
     redirect_to conversation_messages_path(@conversation)
   end
