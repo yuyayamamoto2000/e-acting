4708bfa86010996433aadcdc06971199ba6fde00
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-24 15:12:52 +0900

    CommentControllerのcreate実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,21 @@
 class CommentsController < ApplicationController
+  # コメントを保存、投稿するためのアクションです。
   def create
+    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
+    @work = Work.find(params[:work_id])
+    @comment = @work.comments.build(comment_params)
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@work) }
+      else
+        format.html { redirect_to blog_path(@work), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+  private
+  # ストロングパラメーター
+  def comment_params
+    params.require(:comment).permit(:work_id, :content)
   end
 end
