0d9e0ed4af823137743f0fa2263c089a2bbafcf5
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-24 16:32:30 +0900

    comments.controllerにedit,updateアクション追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_work, only: [:create, :edit, :update]
   # コメントを保存、投稿するためのアクションです。
   def create
     # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
@@ -12,10 +13,35 @@ class CommentsController < ApplicationController
         format.html { redirect_to work_path(@work), notice: '投稿できませんでした...' }
       end
     end
+
+    def edit
+      @comment = @work.comments.find(params[:id])
+      respond_to do |format|
+        flash.now[:notice] = 'コメントの編集中'
+        format.js { render :edit }
+      end
+    end
+
+    def update
+      @comment = @work.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+    end
   end
   private
   # ストロングパラメーター
   def comment_params
     params.require(:comment).permit(:work_id, :content)
+  end
+
+  def set_work
+    @work = Work.find(params[:work_id])
   end
 end
