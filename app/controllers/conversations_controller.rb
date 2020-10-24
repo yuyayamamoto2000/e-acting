939d079b321ac8d79800d7f671f22dfa6badde4d
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-24 17:10:06 +0900

    Conversations.controllerにcreateアクション追加

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,14 @@
 class ConversationsController < ApplicationController
   def index
-  @conversations = Conversation.all
+    @conversations = Conversation.all
+  end
+
+  def create
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?#senderが送り主で、recipientが受取人。
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    else
+      @conversation = Conversation.create!(conversation_params)
+    end
+    redirect_to conversation_messages_path(@conversation)
   end
 end
