b4582e2754bedebac0f709931f60b39f416f95f7
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-24 17:21:52 +0900

    Message.controllerにindexアクション追記

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,20 @@ class MessagesController < ApplicationController
     @conversation = Conversation.find(params[:conversation_id])
   end
 
+  def index
+    @messages = @conversation.messages
+    if @messages.length > 10
+      @over_ten = true
+      @messages = Message.where(id: @messages[-10..-1].pluck(:id))
+    end
+    if params[:m]
+      @over_ten = false
+      @messages = @conversation.messages
+    end
+    if @messages.last
+      @messages.where.not(user_id: current_user.id).update_all(read: true)
+    end
+    @messages = @messages.order(:created_at)
+    @message = @conversation.messages.build
+  end
 end
