0d1fcdc5b017c20984816e75ea77b1d18bd6afd8
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-30 02:52:31 +0900

    修正箇所添削

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Eacting
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.generators do |g|
       g.test_framework :rspec,
