1e247f870de4a0fb2c0a5da4367d1e6c5eba2b0b
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-29 23:07:54 +0900

    ダイレクトメッセージにバリデーション追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
 
+  validates :body, presence: true
+
   validates_presence_of :body, :conversation_id, :user_id
   def message_time
     created_at.strftime("%m/%d/%y at %l:%M %p")
