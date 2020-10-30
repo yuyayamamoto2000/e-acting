1e20222fce0875c1901323ceb994e78d3f7e780d
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-30 11:49:59 +0900

    validete修正

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,6 +3,7 @@ class Work < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
 
+  validate :reward_before_zero, on: :create
   validate :date_before_today, on: :create
   validates :kind, presence: true
   validates :gametitle, presence: true
@@ -14,6 +15,10 @@ class Work < ApplicationRecord
 
   def date_before_today
     errors.add(:deadline, "は本日以降のものを選択してください") if deadline.nil? || deadline < Date.today
+  end
+
+  def reward_before_zero
+    errors.add(:reward, "は0以上のものを選択してください") if reward.nil? || reward < 0
   end
 
   enum method: %i[ 代行 同行]
